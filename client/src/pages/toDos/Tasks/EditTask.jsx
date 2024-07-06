2ee9d4e1c4bcc5ea2871fe22a4fdf1c73c633d9a
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-07-06 18:26:49 +0600

    added design to the edit task

--- client/src/pages/toDos/Tasks/EditTask.jsx
+++ client/src/pages/toDos/Tasks/EditTask.jsx
@@ -1,6 +1,7 @@
-import { useLoaderData } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
 import Input from "../../../components/ui/Input";
 import { useEffect, useState } from "react";
+import Button from "../../../components/ui/Button";
 
 function EditTask() {
   const data = useLoaderData();
@@ -14,15 +15,23 @@ function EditTask() {
   }, [data.task.title]);
 
   return (
-    <div className="flex-[1] bg-white h-fit p-2">
-      <Input
-        type="text"
-        name="title"
-        value={title}
-        onChange={handleTask}
-        classNames="bg-[#e5e5e5]"
-      />
-    </div>
+    <Form className="flex-[3] bg-white h-fit rounded-md overflow-hidden">
+      <h3 className="text-xl font-medium py-2 px-4 bg-[#e5e5e5] ">Edit</h3>
+      <div className="py-4 px-2">
+        <div className="flex items-center gap-2 ">
+          <input type="checkbox" />
+          <input
+            type="text"
+            name="title"
+            onChange={handleTask}
+            value={title}
+            className="input py-1 px-0 h-auto rounded-sm border-t-0 border-r-0 border-l-0 border-b-2 border-[#fca311] w-full font-shantellSans text-xl "
+          ></input>
+        </div>
+        <Button>Cancel</Button>
+        <Button>Update</Button>
+      </div>
+    </Form>
   );
 }
 
