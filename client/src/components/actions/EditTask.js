1f17264c04b7642cf49485ff2b94f4c976b83ffc
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-07-08 03:01:21 +0600

    removed the console log from EditTask.js

--- client/src/components/actions/EditTask.js
+++ client/src/components/actions/EditTask.js
@@ -2,17 +2,14 @@ import { redirect } from "react-router-dom";
 import { handleCompleteTask, updateTask } from "../../services/apiTasks";
 
 export async function action({ request, params }) {
-  console.log("called");
   const { taskId } = params;
   const data = await request.formData();
   const checkTask = data.get("checkTask");
   const title = data.get("title");
   const checked = data.get("checked") === "on";
-  console.log(data);
 
   if (checkTask) {
-    const task = await handleCompleteTask(taskId);
-    console.log(task);
+    await handleCompleteTask(taskId);
 
     return redirect("..");
   }
