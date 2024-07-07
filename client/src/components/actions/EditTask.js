fe7c7c8f663b0b538988e9f39f4f864f597a1e4a
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-07-07 15:11:05 +0600

    get the taskId using params insead of getting the value from onSubmit value

--- client/src/components/actions/EditTask.js
+++ client/src/components/actions/EditTask.js
@@ -2,15 +2,16 @@ import { redirect } from "react-router-dom";
 import { handleCompleteTask } from "../../services/apiTasks";
 
 export async function action({ request, params }) {
+  console.log("called");
+  const { taskId } = params;
   const data = await request.formData();
-  const id = data.get("id");
   const checked = data.get("checked");
 
   if (checked) {
-    const task = await handleCompleteTask(id);
+    const task = await handleCompleteTask(taskId);
     console.log(task);
     return redirect("..");
   }
 
-  return null;
+  return redirect("..");
 }
