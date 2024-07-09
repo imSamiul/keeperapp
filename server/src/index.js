3643a50279f801fb725da885eb2bcca9ff358bf3
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-07-09 16:42:25 +0600

    delete the ip from app.listen

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,6 @@ const app = require('./app');
 
 const port = process.env.PORT;
 
-app.listen(port, '192.168.31.207', () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
