e5a73da77c92f906fa7044b977e74221fe69ed94
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-07-09 19:48:03 +0600

    added server test route

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 
 const port = process.env.PORT;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
