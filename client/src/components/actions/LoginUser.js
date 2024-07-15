5e865a6a21d9d0ece4ea028260b5053ff891d871
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-07-15 17:56:16 +0600

    replace localstorage token with cookies token

--- client/src/components/actions/LoginUser.js
+++ client/src/components/actions/LoginUser.js
@@ -2,6 +2,7 @@ import { redirect } from "react-router-dom";
 import { addUser, setToken } from "../../pages/users/userSlice";
 import { login } from "../../services/apiUsers";
 import store from "../../store";
+import { setAuthToken } from "../../util/auth";
 
 export async function action({ request }) {
   const formData = await request.formData();
@@ -22,7 +23,8 @@ export async function action({ request }) {
   if (res) {
     store.dispatch(addUser(res.user));
     store.dispatch(setToken(res.token));
-    localStorage.setItem("token", res.token);
+    setAuthToken(res.token);
+    // localStorage.setItem("token", res.token);
     return redirect("/todo");
   } else {
     alert(res);
